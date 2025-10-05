aa320978abb0f77b92e586928a0ef8c28979428b
Himani1207 <156164653+Himani1207@users.noreply.github.com>
2025-10-05 23:52:05 +0530

    Add files via upload

---
+++ threading.java
@@ -0,0 +1,10 @@
+public class threading {
+    public static void main(String[] args){
+        System.out.println("Main thread started");
+
+
+        Thread T1=new Thread();
+        
+        System.out.println("Main thread ended");
+    }
+}
