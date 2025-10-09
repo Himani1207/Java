4fb720f6b72511bc1ac9c85ebd0c1ef6e548fb46
Himani1207 <156164653+Himani1207@users.noreply.github.com>
2025-10-10 01:15:36 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,13 @@
+// Literals -->a contant value that is assigned to variable
+public class literals{
+    public static void main(String[] args){
+        byte age=34;
+        char ch='A';
+        float b=5.6f;//floating point hai isliye f use kiya
+        double d1=4.67D;
+        System.out.println(ch);
+        System.out.println(b);
+        System.out.println(d1);
+        System.out.println(age);
+    }
+}
