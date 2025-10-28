d94793f258f1c9d77092fcd1b4a443db095f7542
Himani1207 <156164653+Himani1207@users.noreply.github.com>
2025-10-28 23:55:04 +0530

    Add files via upload

---
+++ MoveZero.java
@@ -0,0 +1,24 @@
+public class MoveZero{
+    public static void main(String[] args){
+        int[] arr={1,0,2,3,2,0,0,4,5,1};
+        int j=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                j=i;
+                break;
+            }
+        }
+
+        for(int i=j+1;i<arr.length;i++){
+           if(arr[i]!=0){
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+            j++;
+           }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
